2968743eaed3fe58303344fa355d2ff20d8b3d36
KubaKrysiak <43858173+KubaKrysiak@users.noreply.github.com>
2019-10-03 19:53:31 +0200

    zajecia

--- scripts.js
+++ scripts.js
@@ -1,1 +1,81 @@
 console.log("skrypty załadowane!")
+
+const nwm22 = [
+{
+     Name: "marek22",
+     kolor: "czerwona",
+     age: 522
+},
+
+
+
+ {
+     Name: "marek11",
+     kolor: "niebieska",
+    age: 511
+ },
+
+
+
+{
+      Name: "marek62",
+     kolor: "zielona",
+     age: 562
+ },
+
+
+
+{
+     Name: "marek23",
+     kolor: "czarna",
+     age: 523
+ },
+
+
+
+ {
+     Name: "marek55",
+     kolor: "czerwona",
+     age: 555
+ },
+
+
+
+{
+      Name: "marek555",
+      kolor: "niebieska",
+       age: 5555
+ },
+
+
+
+ {
+     Name: "marek5",
+     kolor: "zielona",
+     age: 55
+ },
+
+
+
+    {
+         Name: "marek10",
+         kolor: "czarna",
+         age: 10
+     }
+]
+
+
+const owce = nwm22.map(
+    nwm => `
+    <div class="owca">
+        <h3>${nwm.kolor}<h3/>
+    </div>
+    
+    `
+)
+
+const tekst = owce.join("");
+
+const tekst2 = document.querySelector("#nwm")
+
+tekst.innerHTML = tekst2
